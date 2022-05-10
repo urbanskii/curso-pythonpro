8207d6c815b59df67e9cb9a671ce00a6e69dd73d
Marcos Urbanski <marcos.urbanski@gmail.com>
2022-05-10 16:43:19 -0300

    Add desafio_03.py

---
+++ fundamentos_do_python/desafio_03.py
@@ -0,0 +1,11 @@
+#coding: utf-8
+
+
+"""
+Faça um Programa que peça dois números e imprima a soma.
+
+"""
+
+numero1 = int(input("Insira um número: "))
+numero2 =int(input("Insira outro número: "))
+print(f'Soma dos números informados: {numero1 + numero2}')
