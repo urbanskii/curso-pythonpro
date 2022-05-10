0a4f97346a06e4b4fb200c239f15957fcdafd683
Marcos Urbanski <marcos.urbanski@gmail.com>
2022-05-10 16:24:45 -0300

    Add desafio_02.py

---
+++ fundamentos_do_python/desafio_02.py
@@ -0,0 +1,10 @@
+#coding: utf-8
+
+"""
+Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+"""
+
+numero = input('Informe um número: ')
+print(f'O número informado foi {numero}')
+
