95f5a4c5b36989b99a251f732de79d779b581d7f
Marcos Urbanski <marcos.urbanski@gmail.com>
2022-05-10 18:50:27 -0300

    Add desafio_04.py

---
+++ fundamentos_do_python/desafio_04.py
@@ -0,0 +1,12 @@
+#coding: utf-8
+"""
+4. Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+"""
+
+nota_01 = float(input('Informe primeira nota: '))
+nota_02 = float(input('Informe segunda nota: '))
+nota_03 = float(input('Informe terceira nota: '))
+nota_04 = float(input('Informe quarta nota: '))
+
+print(f'Media das notas informadas: {(nota_01 + nota_02 + nota_03 + nota_04)/4}')
