42305427630ab33c8abf5fa11f10432cf3d2369d
Marcos Urbanski <marcos.urbanski@gmail.com>
2022-05-20 19:31:42 -0300

    .

--- fundamentos_do_python/desafio_17.py
+++ fundamentos_do_python/desafio_17.py
@@ -11,6 +11,36 @@
               para cima, isto é, considere latas cheias.
 
 """
+import math
 
-metros = float(input('Informe o tamanho em metros quadrados da área a ser pintada: '))
+metros = float(input('Informe o tamanho em metros quadrados da área a ser pintada: ')) #area em metros quadrados.
+metros_com_folga = metros * 1.1
+
+litros_por_metro = 6
+litros_a_serem_usados = metros_com_folga / litros_por_metro
+litros_por_lata = 18
+numero_de_latas = litros_a_serem_usados / litros_por_lata
+
+
+numero_de_latas = math.ceil(litros_a_serem_usados / litros_por_lata)
+valor_com_apenas_latas = numero_de_latas * 80
+print(f'Você deverá usar {numero_de_latas} latas de 18 litros, no valor de R$ {valor_com_apenas_latas}')
+
+litros_por_galao = 3.6
+numero_de_galoes = math.ceil(litros_a_serem_usados / litros_por_galao)
+valor_com_apenas_galoes = numero_de_galoes * 25
+print(f'Você deverá usar {numero_de_galoes} galões de 3.6 litros, no valor de R$ {valor_com_apenas_galoes}')
+
+
+#Compra de tinta otimizada por valor 
+numero_de_latas = math.floor(litros_a_serem_usados / litros_por_lata)
+valor_de_latas = numero_de_latas * 80
+litros_faltantes = litros_a_serem_usados % litros_por_lata
+numero_de_galoes = math.ceil(litros_faltantes / litros_por_galao)
+valor_com_galoes = numero_de_galoes * 25
+
+valor_total = valor_de_latas + valor_com_galoes
+
+print(f'Você deverá usar {numero_de_latas} latas de 18 litros mais, {numero_de_galoes} galões de 3.6 litros, no valor de R$ {valor_total}')
+
 
