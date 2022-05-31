ff16607ec76f203d589ee14539e9178df9ac76eb
Marcos Urbanski <marcos.urbanski@gmail.com>
2022-05-31 14:14:07 -0300

    add file

---
+++ fundamentos_do_python/ListasPythonBrazil/EstruturaDeDecisao/desafio_01.py
@@ -0,0 +1,17 @@
+"""
+
+1. Faça um Programa que peça dois números e imprima o maior deles.
+
+"""
+
+
+primeiro_numero = int(input('Informe o primeiro numero: '))
+
+segundo_numero = int(input('Informe o segundo numero: '))
+
+if primeiro_numero > segundo_numero:
+    print(f'Primeiro numero é maior: {primeiro_numero}')
+else:
+    print(f'Segundo numero é maior: {segundo_numero}')
+
+
---
+++ fundamentos_do_python/ListasPythonBrazil/EstruturaDeDecisao/desafio_02.py
@@ -0,0 +1,13 @@
+"""
+
+2. Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+"""
+
+
+valor = float(input('Informe o valor: '))
+
+if valor > 0 :
+    print('Valor positivo: {valor}')
+else:
+    print('Valor negativo: {valor}')
